ff66bb245aeefcafe48fc5ef8f7d534c6ee1bdcc
ruisuAr <luismanuelariza@hotmail.com>
2025-05-22 12:49:43 -0500

    Exercising sentences if, cicle for and while

---
+++ calc_perimetro.cpp
@@ -0,0 +1,32 @@
+/*
+Hacer un algoritmo que permita calcular el perímetro de una estructura y
+mostrarla en decímetros, para ello vamos a pedir tres datos en diferentes
+escalas, uno en hectómetros, otro en decámetros y uno en metros. El algoritmo
+deberá hallar a cuanto equivale el valor ingresado en hectómetros a
+decímetros, de decámetros a decímetros y de metros a decímetros, y sumar cada
+uno de los equivalentes porque el algoritmo pide mostrar el resultado en
+decímetros
+*/
+
+#include <iostream>
+using namespace std;
+
+main() {
+  float hecto, deca, metro, dm_hm, dm_Dm, dm_m, perimeter_dm;
+
+  cout << "Ingresa la cantidad en hectometros: ";
+  cin >> hecto;
+  cout << "Ingresa la cantidad en decametros: ";
+  cin >> deca;
+  cout << "Ingresa la cantidad en metros: ";
+  cin >> metro;
+
+  dm_hm = hecto * 1000;  // Convertir hectómetros a decímetros
+  dm_Dm = deca * 100;    // Convertir decámetros a decímetros
+  dm_m = metro * 10;     // Convertir metros a decímetros
+	perimeter_dm = dm_hm + dm_Dm + dm_m;  // Sumar todos los decímetros
+
+	cout << "El perímetro total en decímetros es: " << perimeter_dm << endl;
+
+	return 0;
+}
---
+++ carga_sacas.cpp
@@ -0,0 +1,38 @@
+/*
+Se requiere un programa que establezca un protocolo de carga de
+20 camiones diarios:
+
+●	Capacidad de carga de camiones: entre 18000 kg y 28000 kg
+●	Sacas de cal: entre 3000 kg y 9000 kg
+
+Cada camión debe cargar a su máxima capacidad, si la siguiente saca excede el
+límite de capacidad el camión debe partir y asi hasta completar 20 camiones.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  float cap_camion, acum_sacas, peso_saca;
+
+  cout << "Digite el peso de la primera saca: ";
+  cin >> peso_saca;
+
+  for (int i = 1; i <= 20; i++) {
+    cout << "Digite la capacidad del camion " << i << ": ";
+    cin >> cap_camion;
+
+    acum_sacas = 0;  // Reiniciar acumulador de sacas para cada camión
+
+    while (acum_sacas + peso_saca <= cap_camion) {
+      acum_sacas += peso_saca;  // Acumular el peso de la saca
+
+      cout << "Digite el peso de la siguiente saca: ";
+      cin >> peso_saca;
+    }
+    cout << "El camion numero " << i << " se ha despachado" << endl;
+  }
+  cout << "Se completo la producción del dia, se despacharon 20 camiones";
+
+  return 0;
+}
